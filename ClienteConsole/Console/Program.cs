0e4a2830e890fa7b3d9b946c0cec68a52739077f
danilobreda <danilo@bredas.com.br>
2016-09-08 20:56:20 -0300

    inicio projeto

---
+++ ClienteConsole/Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using Console.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client client = new ServiceReference1.Service1Client();
+
+            // Use a variável 'client' para chamar as operações no serviço.
+            client.Open();
+            do
+            {
+                System.Console.Clear();
+                System.Console.WriteLine("Digite o valor A: ");
+                int a = int.Parse(System.Console.ReadLine());
+                System.Console.WriteLine("Digite o valor B: ");
+                int b = int.Parse(System.Console.ReadLine());
+                var result = client.Soma(a,b);
+                System.Console.WriteLine(result);
+                System.Console.ReadKey();
+            }
+            while(true);
+            // Sempre feche o cliente.
+            client.Close();
+        }
+    }
+}
---
+++ ServidorWCF/WcfDanMatsy/Service1.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfDanMatsy
+{
+    public class Service1 : IService1
+    {
+        public string Soma(int a, int b)
+        {
+            return "A: (" + a.ToString() + ") + B(" + b.ToString() + ") = " + (a + b).ToString();
+        }
+    }
+}
